0fc01ae8c8f0c284250887b5fced2d7d37f6ca79
pravin123-abc <55385439+pravin123-abc@users.noreply.github.com>
2019-09-17 18:30:26 +0530

    adding curly bracket

--- Demo.java
+++ Demo.java
@@ -1,6 +1,7 @@
 class Greet{
 public void message(){
-System.out.println("Welcome");
+     System.out.println("Welcome");
+}
 public static void main(String[] args){
-message();
+      message();
 }
